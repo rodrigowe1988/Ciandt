3c1581188750fb2ad7f43b7e1ba5c78022ecbb36
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-02-17 10:54:37 -0300

    testing changes

---
+++ Angular/alurapic/src/app/photos/photos.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { PhotoComponent } from "./photo/photo.component";
+
+@NgModule({
+    declarations: [ PhotoComponent ],
+    exports: [ PhotoComponent ] 
+})
+
+export class PhotosModule {}
