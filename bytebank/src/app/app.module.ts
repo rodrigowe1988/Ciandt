5da55a8838094d8cde6bff7f3a9f972d8ecf426e
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-02-18 09:39:51 -0300

    bytebank

---
+++ bytebank/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
