022b08fbe6af63516a007d754f8d9c1f7d30264c
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-03-17 06:51:35 -0300

    starting the list course of java alura

--- ANGULAR/memories/src/app/components/item-detail/item-detail.component.ts
+++ ANGULAR/memories/src/app/components/item-detail/item-detail.component.ts
@@ -1,4 +1,8 @@
+import { ListService } from 'src/app/service/list.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Animal } from 'src/app/Animal';
 
 @Component({
   selector: 'app-item-detail',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-detail.component.css']
 })
 export class ItemDetailComponent implements OnInit {
+  animal?: Animal;
 
-  constructor() { }
+  constructor(private listService: ListService, private route: ActivatedRoute) {
+    this.getAnimal();
+  }
 
   ngOnInit(): void {
+  }
+
+  getAnimal() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.listService.getItem(id).subscribe((animal) => (this.animal = animal));
   }
 
 }
