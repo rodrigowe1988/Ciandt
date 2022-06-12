8f7c13066e0a1d97f9978c9e86ac8265bae78261
rodrigo <rodrigoweber97@hotmail.com>
2022-06-12 18:29:10 -0300

    adding with linux mint

---
+++ JAVA/funcoes_lambda/lambda/comparator/src/App.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+        list.add(new Product("Hard Disk", 99.00))
+
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        System.out.println("Imprimindo o getName e o getValue");
+        for (Product prod: list) {
+            System.out.println(prod.getName()+ " " + prod.getValue());
+        }
+
+        System.out.println("Imprimindo usando o método toString da classe Product");
+        for (Product prod: list) {
+            System.out.println(prod);
+        }
+    }
+}
---
+++ JAVA/funcoes_lambda/lambda/comparator/src/Product.java
@@ -0,0 +1,34 @@
+import java.util.Set;
+
+public class Product {
+
+	private String name;
+	private Double value;
+
+	public Product(String name, Double value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "Name: " + getName() + "-> Value:" + getValue();
+	}
+
+}
